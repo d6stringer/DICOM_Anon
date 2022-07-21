1c6222704dcc343a0a7a0aead360b5dae9cc671a
Daniel Woodson <s.dwoodson@gmail.com>
2022-07-21 15:29:16 -0700

    Added anon options

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import os
 import tkinter.messagebox
 import pydicom
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import filedialog, ttk, IntVar
 
 def anonymize():
     #takes path of file containing dicom files
     # delete's paitent's name
     total_files = len(os.listdir(path))
-    global root, pb, value_label
+    global root, pb, value_label, name, bd, other
     n = 0
     for file in os.listdir(path):
         n += 1
@@ -27,17 +27,21 @@ def anonymize():
             # skip over non-dcm files
             if full_path[-4:] == '.dcm':
                 dicomObject = pydicom.read_file(full_path)
-                dicomObject.PatientName = '' #set to blank, could be None
+                if name.get() == 1:
+                    dicomObject.PatientName = '' #set to blank, could be None
+                if bd.get() == 1:
+                    dicomObject.PatientBirthDate = ''
+                if other.get() == 1:
+                    dicomObject.PatientSex = ''
+                    dicomObject.PatientID = ''
                 dicomObject.save_as(full_path)
                 pb['value'] = status
                 value_label.config(text=str(status) + "%")
                 root.update_idletasks()
-
 def user_confirm():
     answer = tkinter.messagebox.askquestion(title='Are you sure?',
                                    message='The DICOM files in\n' + path + '\nwill all be fully and irreversably anonymized!',
-                                   icon='question'
-                                   )
+                                   icon='question')
     if answer == 'yes':
         anonymize()
     else:
@@ -46,7 +50,6 @@ def mkBye():
     tkinter.messagebox.showinfo(title='M-kay',
                                 message='Bye!')
     return
-
 def get_dir():
     global path
     path = filedialog.askdirectory()
@@ -57,28 +60,53 @@ path = ''
 status = 0
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry('500x250')
     root.title('Anonymize DICOMs')
 
     btn1 = tk.Button(root,
                     text= 'Select Folder',
                     command= get_dir)
-    btn1.pack(pady=10)
-
+    btn1.grid(row=0, column=1, pady=10)
     dir_label = ttk.Label(root,
                           text=path)
-    dir_label.pack(pady=10)
+    dir_label.grid(row=1, column=1, pady=10)
+    name = IntVar()
+    bd = IntVar()
+    other = IntVar()
+    cb1 = tk.Checkbutton(root, text='Name',
+                         variable=name,
+                         onvalue=1,
+                         offvalue=0,
+                         height=2)
+    cb1.select()
+    cb2 = tk.Checkbutton(root, text='Birthday',
+                         variable=bd,
+                         onvalue=1,
+                         offvalue=0,
+                         height=2)
+    cb2.select()
+    cb3 = tk.Checkbutton(root, text='Other',
+                         variable=other,
+                         onvalue=1,
+                         offvalue=0,
+                         height=2)
+    cb3.select()
+    cb1.grid(row=2, column=0, pady=10, padx=10)
+    cb2.grid(row=2, column=1, pady=10, padx=10)
+    cb3.grid(row=2, column=2, pady=10, padx=10)
     btn2 = tk.Button(root,
                     text= 'Anonymize',
                     command= user_confirm)
-    btn2.pack(pady=10)
+    btn2.grid(row=3, column=1, pady=10)
+    stat = ttk.Label(root, text='Status')
+    stat.grid(row=4, column=1)
     pb = ttk.Progressbar(root,
                          orient='horizontal',
                          mode='determinate',
                          length=175)
-    pb.pack(pady=10)
+
+    pb.grid(row=5, column=1)
     value_label = ttk.Label(root, text='0%')
-    value_label.pack(pady=10)
+    value_label.grid(row=6, column=1, pady=10)
     root.mainloop()
 
 
